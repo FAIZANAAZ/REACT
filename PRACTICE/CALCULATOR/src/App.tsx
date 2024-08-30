00681b6be0ea645560a637b7673bc4fe359bbec0
FAIZA NAAZ <160342268+FAIZANAAZ@users.noreply.github.com>
2024-08-31 03:23:13 +0500

    first commit

---
+++ PRACTICE/CALCULATOR/src/App.tsx
@@ -0,0 +1,14 @@
+import Hello from "./components/HELLO/hello";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Hello/>
+    </>
+  )
+}
+
+export default App
---
+++ PRACTICE/CALCULATOR/src/components/HELLO/hello.tsx
@@ -0,0 +1,18 @@
+import "./hello.css"
+
+
+// rfce short form
+
+function Hello() {
+    return(
+        <>
+          
+         <p>😊THIS IS REACT WORK 😊</p>
+      
+
+        
+        </>
+    )
+}
+
+export default Hello
